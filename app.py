467a3dbcfcfaf88337bd7e516e650d2b5b02a85c
Eklavya Singh <eklavyasingh9870@gmail.com>
2026-03-28 11:35:27 +0530

    updated

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +13,10 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
